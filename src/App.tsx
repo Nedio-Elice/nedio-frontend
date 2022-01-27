8d17cf7f03dc4c82309e50551bdb0edfce6b5665
Danji-ya <erangerang15@gmail.com>
2022-01-27 14:08:47 +0900

    [setting]: 초기 세팅 1. eslint prettier 적용 2. png, gif 등등 타입 적용 3. emotion 설치

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+import React from 'react';
+import gallery from './assets/icons/gallery.png';
+
+function App() {
+  /* <img src={gallery} alt="hello" /> */
+  return (
+    <div
+      css={css`
+        color: dodgerblue;
+      `}
+    >
+      hello
+    </div>
+  );
+}
+
+export default App;
