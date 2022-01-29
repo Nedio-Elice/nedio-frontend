8bbec41986d13cfc46e9ae485119ec806aefb16e
Danji-ya <erangerang15@gmail.com>
2022-01-29 22:03:20 +0900

    [feat]: Card 컴포넌트 구현

---
+++ src/constants/images.ts
@@ -0,0 +1,25 @@
+import sample from '../assets/images/sample.png';
+import sample2 from '../assets/images/sample2.png';
+import sample3 from '../assets/images/sample3.png';
+import animal from '../assets/images/animal.png';
+import art from '../assets/images/art.png';
+import construct from '../assets/images/construct.png';
+import culture from '../assets/images/culture.png';
+import fashion from '../assets/images/fashion.png';
+import journalism from '../assets/images/journalism.png';
+import nature from '../assets/images/nature.png';
+import person from '../assets/images/person.png';
+
+export {
+  sample,
+  sample2,
+  sample3,
+  animal,
+  art,
+  construct,
+  culture,
+  fashion,
+  journalism,
+  nature,
+  person,
+};
