cfa90440f7fe30a131829bd5aacc73c3e7f06143
Aiden <rheech22@gmail.com>
2022-02-01 01:27:21 +0900

    [feature] GalleryEditPage 상단 폼 추가
    - 갤러리 제목, 분류, 기간, 설명 인풋 추가
    - 테스트 코드 작성

--- src/components/GalleryEdit/index.test.tsx
+++ src/components/GalleryEdit/index.test.tsx
@@ -14,4 +14,19 @@ describe('GalleryEdit', () => {
 
     expect(getByLabelText('포스터 업로드')).not.toBeNull();
   });
+
+  it('renders poster upload interface', () => {
+    const { getByLabelText } = render(<GalleryEdit />);
+
+    expect(getByLabelText('포스터 업로드')).not.toBeNull();
+  });
+
+  it('renders gallery upload interfaces', () => {
+    const { getByLabelText } = render(<GalleryEdit />);
+
+    expect(getByLabelText('제목')).not.toBeNull();
+    expect(getByLabelText('분류')).not.toBeNull();
+    expect(getByLabelText('기간')).not.toBeNull();
+    expect(getByLabelText('설명')).not.toBeNull();
+  });
 });
--- src/components/GalleryEdit/index.tsx
+++ src/components/GalleryEdit/index.tsx
@@ -3,8 +3,31 @@ function GalleryEdit() {
     <div>
       <h1>This is GalleryEditPage</h1>
       <div>
-        <input type="file" id="posterUpload" />
         <label htmlFor="posterUpload">포스터 업로드</label>
+        <input type="file" id="posterUpload" />
+      </div>
+      <div>
+        <label htmlFor="title">제목</label>
+        <input type="text" id="title" />
+      </div>
+      <div>
+        <label htmlFor="category">분류</label>
+        <select id="category">
+          <option value="">카테고리를 선택해주세요</option>
+          <option value="자연">자연</option>
+          <option value="인물">인물</option>
+          <option value="동물">동물</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="date">기간</label>
+        <input type="date" id="date" />
+        -
+        <input type="date" id="date" />
+      </div>
+      <div>
+        <label htmlFor="description">설명</label>
+        <input type="textarea" id="description" />
       </div>
     </div>
   );
