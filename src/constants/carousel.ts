dabd56167298957afb2a30154c5faf34deb38de4
Danji-ya <erangerang15@gmail.com>
2022-02-02 18:34:07 +0900

    chore: folder 대소문자 변경

---
+++ src/constants/carousel.ts
@@ -0,0 +1,14 @@
+const CAROUSEL = {
+  PADDING_DATA: 2,
+  PADDING: 80,
+  TRANSITION_TIME: 500,
+  HANDLE_TYPE: {
+    PREV: -1,
+    NEXT: 1,
+  },
+  ITEM_MAX_WIDTH: 1250,
+  ITEM_MIN_WIDTH: 560,
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { CAROUSEL };
