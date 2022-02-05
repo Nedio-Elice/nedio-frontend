ad79519ff277e0966acc8aba082c6a1d325e725f
Aiden <rheech22@gmail.com>
2022-02-05 09:49:11 +0900

    [feat]: 카테고리 추가
    1. 8개 항목 모두 추가

--- src/components/GalleryEdit/Categories.tsx
+++ src/components/GalleryEdit/Categories.tsx
@@ -33,6 +33,11 @@ function Categories({ onChange, category }: Props) {
         <option value="자연">자연</option>
         <option value="인물">인물</option>
         <option value="동물">동물</option>
+        <option value="예술">예술</option>
+        <option value="문화">문화</option>
+        <option value="건축">건축</option>
+        <option value="패션">패션</option>
+        <option value="저널리즘">저널리즘</option>
       </select>
     </Container>
   );
