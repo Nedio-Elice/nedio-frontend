63d65fa76fbb09175700c718629ec211eefc4758
이창혁 <rheech22@gmail.com>
2022-02-07 02:56:57 +0000

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const url = process.env.SERVER_URL || 'http://localhost:4000/api/';
+export const url = process.env.SERVER_URL || 'http://localhost:4000/api';
 
 export const axiosInstanceFormData = axios.create({
   baseURL: url,
